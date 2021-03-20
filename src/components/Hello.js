0c4ec99ad4167b9d8e17f858db528f673e923d77
Brandon <brandonbgrayson@gmail.com>
2021-03-19 21:11:56 -0400

    added hello componenet

---
+++ src/components/Hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from The Hello Component</h1>
+            </div>
+        )
+        
+    }
+};
+
+export default Hello;
+
+
